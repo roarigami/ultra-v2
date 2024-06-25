80160b88d5552315fc27aea5cbd221c383fd5943
Rory Kane <roarigami@gmail.com>
2024-06-25 16:08:11 -0400

    Game Loop
    Game control constructor grabs canvas element through the gamecontainer object that is passed as an argument when the gamecontrol class is instantiated. getContext method is used to create a draw context on the canvas.

---
+++ js/gamecontrol.js
@@ -0,0 +1,15 @@
+class GameControl {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = canvasUV2;
+        this.height = canvasUV2.height;
+        this.width = canvasUV2.width;
+        this.ctx = this.canvas.getContext('2d', {willReadFrequently: true});
+        this.map = null;
+    }
+
+
+
+
+
+}
