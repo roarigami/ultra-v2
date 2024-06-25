bb30960d1e67d63a52d2dfce03c6b8a542a108cc
Rory Kane <roarigami@gmail.com>
2024-06-25 15:56:48 -0400

    Game control setup
    game control class is initialised on page load

--- js/init.js
+++ js/init.js
@@ -2,5 +2,9 @@
 Ultra V2 Initialisation
 ***********************/
 window.addEventListener('load', function() {
-
+  loading.style.display = 'none';
+  const gamecontrol = new GameControl({
+      element: gameContainer
+  });
+  gamecontrol.init();
 });
