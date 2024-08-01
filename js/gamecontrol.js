427274a40bd533b2882089b43b19d048f3317056
Rory Kane <roarigami@gmail.com>
2024-08-01 12:49:28 -0400

    Gravity test
    Incrementing y coordinate with animation frame to test vertical movement for drip effect

--- js/gamecontrol.js
+++ js/gamecontrol.js
@@ -6,11 +6,16 @@ class GameControl {
         this.width = canvasUV2.width;
         this.ctx = this.canvas.getContext('2d', {willReadFrequently: true});
         this.map = null;
+
+        this.y = 100;
     }
 
     startGameLoop(context) {
       const animate = () => {
         context.clearRect(0, 0, this.width, this.height);
+        context.fillRect(200, this.y, 100, 100);
+        requestAnimationFrame(animate);
+        this.y++;
       }
       animate(0);
     }
